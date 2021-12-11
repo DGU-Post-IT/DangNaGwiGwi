7023c69474b5233963e30acaf3fc850819070934
jibminJung <jibminjung@gmail.com>
2021-12-11 17:09:40 +0900

    add table layout width shrink

--- app/src/main/java/com/example/postit/RecordHistoryAdapter.java
+++ app/src/main/java/com/example/postit/RecordHistoryAdapter.java
@@ -75,9 +75,19 @@ public class RecordHistoryAdapter extends RecyclerView.Adapter<RecordHistoryAdap
             }else if(data.getQuestionId()==3){
                 binding.questionTextView.setText("가장 뿌듯했던 순간은 언제였나요?");
             }else if(data.getQuestionId()==4){
-                binding.questionTextView.setText("만약 내일 해외여행을 갈 수 있다면 \n어느나라에 가고 싶나요?");
+                binding.questionTextView.setText("만약 내일 해외여행을 갈 수 있다면 어느나라에 가고 싶나요?");
+            }else if(data.getQuestionId()==5){
+                binding.questionTextView.setText("오늘 가장 재밌었던 일이 뭔가요?");
+            }else if(data.getQuestionId()==6){
+                binding.questionTextView.setText("가장 좋아하는 음식이 뭐예요?\n 누구랑 같이 먹고싶나요?");
+            }else if(data.getQuestionId()==7){
+                binding.questionTextView.setText("어렸을 때 꿈이 뭐였어요?");
+            }else if(data.getQuestionId()==8){
+                binding.questionTextView.setText("보고싶은 친구들이 있나요? \n친구들이랑 주로 뭐하고 노셨어요?");
+            }else if(data.getQuestionId()==9){
+                binding.questionTextView.setText("좋아하는 색깔이뭐예요?");
             }else{
-                binding.questionTextView.setText("나에게 가장 잘해줬던 사람은 누구인가? \n 그 사람에게 어떤 선물을 주고 싶나요?");
+                binding.questionTextView.setText("알 수 없는 질문입니다.");
             }
             binding.answerTextView.setText(data.getAnswer());
 
